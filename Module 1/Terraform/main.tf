42c110843fd66264b6560cd4819d39cfd37381db
callumwhyte <68275842+callumwhyte@users.noreply.github.com>
2026-01-26 19:49:45 +0000

    Updates to terraform docs

--- Module 1/Terraform/main.tf
+++ Module 1/Terraform/main.tf
@@ -12,3 +12,27 @@ provider "google" {
   project     = "de-terraform-485418"
   region      = "europe-west2"
 }
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "de-terraform-485418-demo-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
